4bc3c24b42e7dfa43dc485ae7988e889ef964e66
Andrew Birmingham <andrew_birmingham@msn.com>
2025-04-06 19:56:51 -0600

    working on e2e still

--- cypress/e2e/quiz.cy.ts
+++ cypress/e2e/quiz.cy.ts
@@ -1,18 +1,25 @@
 describe('Quiz e2e test', () => {
   beforeEach( () => {
     cy.visit('/');
+    cy.fixture('questions.json').as('questions');
   });
 
-  it('On load start button is visible'), () => {
-    cy.get('button')
-    .contains('Start Quiz')
-    .should('be.visible');
-  } 
+  it('On load start button is visible', () => {
+    cy.get('button').should('have.text', 'Start Quiz');
+  });
 
-  //it('On click of start button quiz starts')
+  it('On click of start button quiz starts', () => {
+    cy.contains('Start Quiz').click(); // Click the start button
 
-  //it('User is able to complete the quiz')
+    cy.get('@questions').then((questions) => {
+      const firstQuestion = questions[0].question;
+      cy.contains(firstQuestion).should('be.visible');
+  });
+});
+  // it('User is able to complete the quiz')
+  //   cy.wait( )
+
 
   //it('User is able to take a new quiz')
 
-})
+});
